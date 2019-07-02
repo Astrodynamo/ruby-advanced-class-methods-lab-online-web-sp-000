356549a0fa6405bd243c7a4efbce9aaf5d19b55d
Amanda King <amapickell@gmail.com>
2019-07-02 00:45:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,21 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    new_song = self.new
+    self.all << new_song
+    new_song
+  end
+  
+  def self.new_by_name (name)
+    new_song_by_name = self.new
+    new_song_by_name.name = name
+    new_song_by_name
+  end
+  
+  def self.create_by_name (name)
+    self.all << self.new_by_name(name)
+  end
 
 end
