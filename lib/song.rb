b342e61fbf657219dd3ab896b16b773d62047ec4
Amanda King <amapickell@gmail.com>
2019-07-02 00:58:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
   
   def self.find_by_name (name)
-    self.all.each {|song| song.name == name ? song : false}
+    self.all.each {|song| song.name == name ? return song : return false}
   end
 
 end
