1cfa03195e4b3bbfa193ab7914eec4e782d2b1ae
Amanda King <amapickell@gmail.com>
2019-07-02 01:23:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,13 @@ class Song
   end
   
   def self.create_from_filename (filename)
-    
+    data = filename.split(" - ")
+    artist_name = data[0]
+    song_name = data[1].chomp(".mp3")
+    new_song = self.new_by_name(song_name)
+    new_song.artist_name = artist_name
+    self.all << new_song
+    new_song
   end
 
 end
