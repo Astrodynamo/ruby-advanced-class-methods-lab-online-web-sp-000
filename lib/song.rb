d2baabc61d2650fccf454dc9ad3d759255fb9cc4
Amanda King <amapickell@gmail.com>
2019-07-02 01:26:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,9 +53,8 @@ class Song
     data = filename.split(" - ")
     artist_name = data[0]
     song_name = data[1].chomp(".mp3")
-    new_song = self.new_by_name(song_name)
+    new_song = self.create_by_name(song_name)
     new_song.artist_name = artist_name
-    self.all << new_song
     new_song
   end
   
